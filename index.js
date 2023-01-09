8cebc385f9bb1b25a8c1b7946a0ad92633c42c03
Mariia Khimochkina <maria-himoch@yandex.ru>
2023-01-09 16:31:09 +0100

    postDataAlmostDone

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     const DATA = [
         {
             projekt: 'First',
-            activity: 'Fusce',
+            activity: 'Drama',
             from: '2:00',
             to: '16:00',
             note: 'Proin vel iaculis diam',
@@ -16,7 +16,7 @@
         },
         {
             projekt: 'Hello',
-            activity: 'Fusce',
+            activity: 'Jumping',
             from: '2:00',
             to: '16:00',
             note: 'Proin vel iaculis diam',
@@ -24,26 +24,42 @@
         }
     ]
 
-    let selectedProject; // value 
+    let selectedProject;
     let selectedAktivitet;
     let timeFrom;
     let timeTo;
     let isBillable;
     let writtenNote;
 
-    const getValueFromAnyInput = (inputValue, inputIdName) => {
-        const anyInput = document.getElementById(inputIdName);
-        anyInput.addEventListener('change', (event) => {
-            inputValue = anyInput.value
-        });
-    }
+    const projectInput = document.getElementById('project');
+    projectInput.addEventListener('change', (event) => {
+        selectedProject = projectInput.value
+    });
 
-    getValueFromAnyInput(selectedProject, 'project');
-    getValueFromAnyInput(selectedAktivitet, 'activity');
-    getValueFromAnyInput(timeFrom, 'from');
-    getValueFromAnyInput(timeTo, 'to');
-    getValueFromAnyInput(isBillable, 'billable');
-    getValueFromAnyInput(writtenNote, 'textarea');
+    const activityInput = document.getElementById('activity');
+    activityInput.addEventListener('change', (event) => {
+        selectedActivity = activityInput.value
+    });
+
+    const fromInput = document.getElementById('from');
+    fromInput.addEventListener('change', (event) => {
+        timeFrom = fromInput.value
+    });
+
+    const toInput = document.getElementById('to');
+    toInput.addEventListener('change', (event) => {
+        timeTo = toInput.value
+    });
+
+    const billableInput = document.getElementById('billable');
+    billableInput.addEventListener('change', (event) => {
+        isBillable = billableInput.value
+    });
+    const textareaInput = document.getElementById('textarea');
+    textareaInput.addEventListener('change', (event) => {
+        writtenNote = textareaInput.value
+    });
+
 
     const getDataFromInputs = () => {
         const newRecord = {
@@ -61,8 +77,24 @@
     sparaButton.addEventListener("click", (event) => {
         getDataFromInputs()
         postData()
+        clearAllInputs()
         event.preventDefault();
     })
+
+    const clearAllInputs = () => {
+        projectInput.value = ''
+        activityInput.value = ''
+        fromInput.value = ''
+        toInput.value = ''
+        billableInput.value = ''
+        textareaInput.value = ''
+        selectedProject = undefined
+        selectedAktivitet = undefined
+        timeFrom = undefined
+        timeTo = undefined
+        isBillable = undefined
+        writtenNote = undefined
+    }
 
     const postData = () => {
         const tbody = document.getElementsByTagName('tbody')[0]
@@ -70,7 +102,7 @@
 
         DATA.map(record => tbody.innerHTML += `<tr>
         <td>${record.projekt}</td>
-        <td>F${record.activity}</td>
+        <td>${record.activity}</td>
         <td>${record.from}</td>
         <td>${record.to}</td>
         <td>${record.note}</td>
